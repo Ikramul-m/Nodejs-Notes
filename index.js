fdfd239a131099c83c930e0d6a52388dd1d6cbfc
Ikramul-m <i20727172@gmail.com>
2023-10-27 20:37:41 +0530

    noteees

--- index.js
+++ index.js
@@ -20,6 +20,24 @@ fs.readFile("./sample.txt","utf-8",(err,data)=>{
 console.log("I am first!!!")
 
 
+const x = "THIS IS DONE BY ME";
+
+fs.writeFile("./sampleTwo.txt", x, () => {
+    console.log("Written")
+})
+
+
+
+fs.readFile("./sampleTwo.txt", "utf-8", (err,data) =>{
+    if(err){
+        throw err
+    }
+    
+    console.log(data)
+})
+
+
+
 
 
 
